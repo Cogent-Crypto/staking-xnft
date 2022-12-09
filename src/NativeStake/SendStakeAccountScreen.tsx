65592ee885b852af84245a1e20c96a641e7af660
benhawkins18 <benhawkins18@gmail.com>
2022-12-09 13:21:52 -0700

    update margin top on send screen

--- src/NativeStake/SendStakeAccountScreen.tsx
+++ src/NativeStake/SendStakeAccountScreen.tsx
@@ -134,7 +134,7 @@ export function SendStakeAccountScreen({ stakeAccount, validator }: { stakeAccou
           value={destinationAddress}
           style={{ width: "100%" }}
         ></TextField>
-        <Text style={{ fontSize: "10px" }}>
+        <Text style={{ fontSize: "10px" }} tw="mt-2">
           Sending a stake account permanently transfers the withdraw and delegation authority to the desination address
         </Text>
       </View>
