6344c0a0a0283984823e22412d1e255f6d2ec2a5
benhawkins18 <benhawkins18@gmail.com>
2023-01-23 10:46:32 -0700

    update jito commission inclusion

---
+++ src/utils/jitoValidatorsMockAPI.ts
@@ -0,0 +1,152 @@
+export let jitoValidators = {
+    "CogentC52e7kktFfWHwsqSmr8LiS1yAtfqhHcftCPcBJ": {
+        "commission": "6"
+    },
+    "juicQdAnksqZ5Yb8NQwCLjLWhykvXGktxnQCDvMe6Nx": {
+        "commission": "0"
+    },
+    "1234LB7uvDC23rdCQoK8C3jNwnovUNyeKxz8wC3dghJ5": {
+        "commission": "5"
+    },
+    "6JfBwvcz5QUKQJ37BMKTLrf968DDJBtwoZLw19aHwFtQ": {
+        "commission": "7"
+    },
+    "RBFvvcGPBpgkBYmJGsphoDQJD8sszSuorM7TorWm12Y": {
+        "commission": "6"
+    },
+    "J1to1yufRnoWn81KYg1XkTWzmKjnYSnmE2VY8DGUJ9Qv": {
+        "commission": "4"
+    },
+    "EARNynHRWg6GfyJCmrrizcZxARB3HVzcaasvNa8kBS72": {
+        "commission": "5"
+    },
+    "9sWYTuuR4s12Q4SuSfo5CfWaFggQwA6Z8pf8dWowN5rk": {
+        "commission": "10"
+    },
+    "8FPz3JG4E3HVXxGbPZVibarva4AGXSZWx3qKLUS5uFtN": {
+        "commission": "0"
+    },
+    "27MtjMSAQ2BGkXNuJEJkxFyCJT8dugGAaHJ9T7Gc6x4x": {
+        "commission": "6"
+    },
+    "GA2t11gJcmuZ4y7pShTzgYDkxVaJaVQJqkVUqojhPPsT": {
+        "commission": "9"
+    },
+    "9QU2QSxhb24FUX3Tu2FpczXjpK3VYrvRudywSZaM29mF": {
+        "commission": "7"
+    },
+    "GE6atKoWiQ2pt3zL7N13pjNHjdLVys8LinG8qeJLcAiL": {
+        "commission": "5"
+    },
+    "J1to2NAwajc8hD6E6kujdQiPn1Bbt2mGKKZLY9kSQKdB": {
+        "commission": "4"
+    },
+    "CAf8jfgqhia5VNrEF4A7Y9VLD3numMq9DVSceq7cPhNY": {
+        "commission": "7"
+    },
+    "ASe5VJAvTWqPeZXaoKZFk23xzoD3SgXZk1JrkWQx3M1W": {
+        "commission": "6"
+    },
+    "FcZ4eXb2NKG5ybLcrY9JjioDwxAVGTnsoFRvtmEpwzGo": {
+        "commission": "4"
+    },
+    "8ELJd5pyMH6VGpFjpDTMFiPVi1VxCpnqWy3B81nL5eTt": {
+        "commission": "4"
+    },
+    "5XdXohV5nSKUmsxrE5qVmpKh32xJKdg7rbsxz2iR4dJ4": {
+        "commission": "4"
+    },
+    "Bkskrv38Kn7zJR5mvmbCDGn2M4Jyhzt2ZqwQXV6rYnXa": {
+        "commission": "100"
+    },
+    "Dn2cRSWAfQpb3NyUJ2q33t1scBLxzo8TZBAyKsWhX7zh": {
+        "commission": "100"
+    },
+    "BDn3HiXMTym7ZQofWFxDb7ZGQX6GomQzJYKfytTAqd5g": {
+        "commission": "10"
+    },
+    "J1to3PQfXidUUhprQWgdKkQAMWPJAEqSJ7amkBDE9qhF": {
+        "commission": "4"
+    },
+    "BWkvytz3MAiLkUbMuYK5yV1VYThbBYYQYG3gdef8NLw5": {
+        "commission": "10"
+    },
+    "ExArtqMtiiuwjReNHtjjuA246ydbvdKoyt7dMnjdpmpB": {
+        "commission": "10"
+    },
+    "9GJmEHGom9eWo4np4L5vC6b6ri1Df2xN8KFoWixvD1Bs": {
+        "commission": "5"
+    },
+    "C2KtdF4FBNybpJrAW9p57SRxv1cqH7EAFeJPJ9J3biLi": {
+        "commission": "10"
+    },
+    "F1FkySZudpAUwfzcgrGVJTrQKFGg2bxBMJooNhRX1RBB": {
+        "commission": "5"
+    },
+    "CrLn7zEBytbmRBUGhkDyyUbGCa6H7bMCnw94Dip8QbcJ": {
+        "commission": "7"
+    },
+    "LodezVTbz3v5GK6oULfWNFfcs7D4rtMZQkmRjnh65gq": {
+        "commission": "0"
+    },
+    "DfpdmTsSCBPxCDwZwgBMfjjV8mF8xHkGRcXP8dJBVmrq": {
+        "commission": "7"
+    },
+    "7K8DVxtNJGnMtUY1CQJT5jcs8sFGSZTDiG7kowvFpECh": {
+        "commission": "6"
+    },
+    "4B592mQ6JbxPwpa3iY3nDgLH9vzppRgG54Y3ZtMrYcSR": {
+        "commission": "10"
+    },
+    "J3C6sLyRYFhuADwTzUTC8ev1tTHKJV1CryxZVHX8mz2C": {
+        "commission": "10"
+    },
+    "9Gko8QZBbV5SrEvHKtQHcMrGGSfgFP3KJUozEGifu25x": {
+        "commission": "1"
+    },
+    "5yHqB3NxovCEMUniQCboaPRMyyQ7kQQF4QqvC4vaz78z": {
+        "commission": "0"
+    },
+    "F2bcoVhE2he5DCruN4PKkWAriNXs2VWty94n9CCdWZ8s": {
+        "commission": "8"
+    },
+    "48oxpSHQkM4sdXUY9NQ8KnEtebzZbyk8uUT7JRdVQNuf": {
+        "commission": "10"
+    },
+    "GuxBSrv5jnSwwPepkqnmkM7YCBSakKanbnw4BKMdda4j": {
+        "commission": "8"
+    },
+    "P4f3F3VfMhKvpGQXg2MuvLfWmZui41gvcH9XKtYDiFX": {
+        "commission": "10"
+    },
+    "G8GqFzoEZrxHqV279nYxD5PX1vWWHvxWHkBDkXU2qMy6": {
+        "commission": "10"
+    },
+    "RAREcr3SmDePhWD4Je1qEh1UMCQD5MFaDHF7UTzezQh": {
+        "commission": "4"
+    },
+    "CzmqDuqEpfnkptuLAcikmJrhCnhFXo8aUBj6Rto1SPAc": {
+        "commission": "10"
+    },
+    "8RDsLp5kLZEt2swpKne8JuZAN6PFRX1ak29Hci2DT1Xa": {
+        "commission": "3"
+    },
+    "AS3nKBQfKs8fJ8ncyHrdvo4FDT6S8HMRhD75JjCcyr1t": {
+        "commission": "0"
+    },
+    "AuBB9st3RqhHBkzZgBSm6SVnHZNJQSHeBWCSkik4bzdA": {
+        "commission": "0"
+    },
+    "StepeLdhJ2znRjHcZdjwMWsC4nTRURNKQY8Nca82LJp": {
+        "commission": "7"
+    },
+    "Luck3DN3HhkV6oc7rPQ1hYGgU3b5AhdKW9o1ob6AyU9": {
+        "commission": "6"
+    },
+    "DsiG71AvUHUEo9rMMHqM9NAWQ6ptguRAHyot6wGzLJjx": {
+        "commission": "0"
+    },
+    "39xF5qkfK5HBaG4Hkq6bjumUB2k4B5ozEAmfZoobhUVw": {
+        "commission": "10"
+    }
+}
