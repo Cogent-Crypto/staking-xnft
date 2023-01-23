2bdae976631fea7aeeea5008355dc1dc0a860e24
benhawkins18 <benhawkins18@gmail.com>
2023-01-23 09:47:21 -0700

    lots of refactoring and getting deposit to work

--- src/LiquidStake/LiquidStakeDetail.tsx
+++ src/LiquidStake/LiquidStakeDetail.tsx
@@ -20,12 +20,69 @@ export function LiquidStakeDetail({ stakePool }: { stakePool: StakePool }) {
     const balance = tokenBalances?.get(stakePool.tokenMint.toString())
 
     const THEME = useCustomTheme();
+    const solbalance = useSolBalance();
 
     const nav = useNavigation();
     const publicKey = usePublicKey();
     // const connection = useConnection();
+    
+
+
+    return (
+        <View tw="text-bold px-2" style={{
+            fontSize: "1rem",
+            fontWeight: "bold",
+            color: THEME.colors?.fontColor,
+            display: "flex",
+            flexDirection: "column"
+
+        }}>
+            <View tw="flex items-center mx-auto px-2 pt-4 text-center  w-4/5 cursor-pointer">
+                <Image style={{ height: "50px", maxWidth: "unset", borderRadius: "999px" }} src={stakePool.tokenImageURL} />
+                <Text tw="mx-auto px-1 text-lg">
+                    {stakePool.poolName}
+                </Text>
+                <View tw="leading-none">
+                    <Text tw="text-green-500 text-md">
+                        {balance?.toFixed(2) || 0}
+                    </Text>
+                    <Text style={{ color: "#A9A9A9", fontSize: "1rem" }}>
+                        {stakePool.tokenSymbol}
+                    </Text>
+                </View>
+            </View>
+            {/* <View tw={`flex items-center mt-4`}>
+                {stakePool.MEVDelegation ? <CheckIcon /> : <RedXIcon />}
+                <Text tw="ml-2">
+                    MEV Enabled
+                </Text>
+
+            </View> */}
+
+
+
+            <TabLayout stakePool={stakePool} solBalance={solbalance} tokenBalance={balance!} />
+            {/* <View tw={`flex items-center mt-4`}>
+                <TextField onChange={(v) => setAmount(v.target.value)} />
+                <Button onClick={() => depositStake(connection, stakePool.poolPublicKey, amount)} tw="mt-4">Stake</Button>
+            </View>
+            <Button tw="mt-4" onClick={() => deposit(stakePool, amount)}>Deposit</Button>
+
+            {balance && balance > 0 &&
+            <Button onClick={() => withdrawStake(connection, stakePool.poolPublicKey, amount)} tw="mt-4">Unstake</Button>} */}
+        </View>
+    )
+}
+
+const TabLayout = ({stakePool, tokenBalance, solBalance }: {stakePool:StakePool,tokenBalance: number, solBalance: number } )=> {
+    const THEME = useCustomTheme();
+    const [stakeAmount, setStakeAmount] = React.useState(solBalance);
+    const [unStakeAmount, setUnStakeAmount] = React.useState(tokenBalance);
+    const publicKey = usePublicKey();
+
+    const [tabIndex, setTabIndex] = React.useState(0);
+
     const connection = new Connection("https://patient-aged-voice.solana-mainnet.quiknode.pro/bbaca28510a593ccd2b18cb59460f7a43a1f6a36/");
-    const solbalance = useSolBalance();
 
 
 
@@ -33,7 +90,7 @@ export function LiquidStakeDetail({ stakePool }: { stakePool: StakePool }) {
         try {
             await window.xnft.solana.sendAndConfirm(transaction)
         } catch (error) {
-            console.log("Here is the error", error);
+            console.log("failed to send stakepool txn:", JSON.stringify(transaction), error);
             return
         }
     }
@@ -76,7 +133,7 @@ export function LiquidStakeDetail({ stakePool }: { stakePool: StakePool }) {
         submitTransaction(txn)
     }
 
-    async function depositSPLTransaction(pool: StakePool, lamports: number) {
+    async function depositSPLPoolTransaction(pool: StakePool, lamports: number) {
         //amount needs to be lamports
         // different logic if marinade need to be implemented
         // stakePoolInfo(connection, pool.poolPublicKey).then(console.log).catch(console.log);
@@ -92,68 +149,16 @@ export function LiquidStakeDetail({ stakePool }: { stakePool: StakePool }) {
         transaction.add(...stakePoolinstruction.instructions);
         transaction.partialSign(stakePoolinstruction.signers[0]);
         // connection.simulateTransaction(transaction).then(console.log).catch(console.log); //this works
-        let txnSignature: any
-        try {
-            txnSignature = await window.xnft.solana.sendAndConfirm(transaction)
-        } catch (error) {
-            console.log("Here is the error", error);
-            return
-        }
+        return transaction
     }
 
-
-    return (
-        <View tw="text-bold px-2" style={{
-            fontSize: "1rem",
-            fontWeight: "bold",
-            color: THEME.colors?.fontColor,
-            display: "flex",
-            flexDirection: "column"
-
-        }}>
-            <View tw="flex items-center mx-auto px-2 pt-4 text-center  w-4/5 cursor-pointer">
-                <Image style={{ height: "50px", maxWidth: "unset", borderRadius: "999px" }} src={stakePool.tokenImageURL} />
-                <Text tw="mx-auto px-1 text-lg">
-                    {stakePool.poolName}
-                </Text>
-                <View tw="leading-none">
-                    <Text tw="text-green-500 text-md">
-                        {balance?.toFixed(2) || 0}
-                    </Text>
-                    <Text style={{ color: "#A9A9A9", fontSize: "1rem" }}>
-                        {stakePool.tokenSymbol}
-                    </Text>
-                </View>
-            </View>
-            {/* <View tw={`flex items-center mt-4`}>
-                {stakePool.MEVDelegation ? <CheckIcon /> : <RedXIcon />}
-                <Text tw="ml-2">
-                    MEV Enabled
-                </Text>
-
-            </View> */}
-
-
-
-            <TabLayout solbalance={solbalance} tokenBalance={balance} />
-            {/* <View tw={`flex items-center mt-4`}>
-                <TextField onChange={(v) => setAmount(v.target.value)} />
-                <Button onClick={() => depositStake(connection, stakePool.poolPublicKey, amount)} tw="mt-4">Stake</Button>
-            </View>
-            <Button tw="mt-4" onClick={() => deposit(stakePool, amount)}>Deposit</Button>
-
-            {balance && balance > 0 &&
-            <Button onClick={() => withdrawStake(connection, stakePool.poolPublicKey, amount)} tw="mt-4">Unstake</Button>} */}
-        </View>
-    )
-}
-
-const TabLayout = ({ tokenBalance, solbalance }) => {
-    const THEME = useCustomTheme();
-    const [stakeAmount, setStakeAmount] = React.useState(solbalance);
-    const [unStakeAmount, setUnStakeAmount] = React.useState(tokenBalance);
-
-    const [tabIndex, setTabIndex] = React.useState(0);
+    async function depositSPLPool(pool: StakePool, lamports: number) {
+        //amount needs to be lamports
+        // different logic if marinade need to be implemented
+        console.log("depositing", lamports)
+        let txn = await depositSPLPoolTransaction(pool,lamports)
+        submitTransaction(txn);
+    }
 
     return (
         <View>
@@ -168,9 +173,9 @@ const TabLayout = ({ tokenBalance, solbalance }) => {
                     <View tw={`w-full`}>
                         <TextField value={stakeAmount} onChange={(v) => setStakeAmount(v.target.value)} />
                         <View tw={`flex items-baseline mt-2`}>
-                            <View tw={`mr-auto`}>Balance: {solbalance / LAMPORTS_PER_SOL} SOL</View><Button onClick={() => setStakeAmount(solbalance / LAMPORTS_PER_SOL - .05)} tw={`text-sm cursor-pointer`}>Max</Button>
+                            <View tw={`mr-auto`}>Balance: {solBalance / LAMPORTS_PER_SOL} SOL</View><Button onClick={() => setStakeAmount(solbalance / LAMPORTS_PER_SOL - .05)} tw={`text-sm cursor-pointer`}>Max</Button>
                         </View>
-                        <Button onClick={() => depositStake(connection, stakePool.poolPublicKey, amount)} tw="mt-60 w-full">Stake SOL</Button>
+                        <Button onClick={() => depositSPLPool(stakePool, stakeAmount*LAMPORTS_PER_SOL)} tw="mt-60 w-full">Stake SOL</Button>
                     </View>
                 }
                 {
